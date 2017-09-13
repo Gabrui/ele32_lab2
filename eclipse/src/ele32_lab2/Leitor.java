3222d3ff0d7b40c7411bf41bd5a9eb63bf0dccc0
Gabriel Adriano de Melo <gaadrime.melo@gmail.com>
2017-09-13 12:52:03 -0300

    Inicio do Projeto do eclipse

---
+++ eclipse/src/ele32_lab2/Leitor.java
@@ -0,0 +1,5 @@
+package ele32_lab2;
+
+public class Leitor {
+
+}
---
+++ eclipse/testes/ele32_lab2/TesteLeitor.java
@@ -0,0 +1,19 @@
+package ele32_lab2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TesteLeitor {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
